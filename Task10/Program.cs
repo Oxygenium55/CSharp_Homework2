a5df6676d418baad4d103318b655f582ac43d469
Oxygen <grandnorik@mail.ru>
2022-10-16 15:24:51 +0600

    Task10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int first2Digits = number / 10;
+int secondDigit = first2Digits % 10;
+
+if(number > 99 && number < 1000)
+{
+    Console.WriteLine($"Вторая цифра числа {number} - {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Введенное число не является трехзначным");
+}
