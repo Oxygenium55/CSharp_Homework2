989d1f44c112cbd4b8dc3b253691e9669fdbca8b
Oxygen <grandnorik@mail.ru>
2022-10-16 21:21:54 +0600

    Task15 done

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine());
+
+if(day == 6 || day == 7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+else
+{
+    Console.WriteLine("Это будний день");
+}
